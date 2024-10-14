9d3ce74b996625ccd07de5197c5f11794434409d
kishorewolfe <kishorewolfeik@gmail.com>
2024-10-14 20:57:55 +0530

    Import changes

--- app/components/Propertycard/PropertySwiper.tsx
+++ app/components/Propertycard/PropertySwiper.tsx
@@ -1,8 +1,6 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 // Import Swiper styles
-import "swiper/css";
 import { useRef } from "react";
 import "./Property.css";
 
